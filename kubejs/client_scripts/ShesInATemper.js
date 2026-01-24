40cdcb304785121b573cff1f139132647da05582
zyszNOI <mcvmware@Hotmail.com>
2026-01-24 08:57:20 +0800

    临行

---
+++ kubejs/client_scripts/ShesInATemper.js
@@ -0,0 +1,9 @@
+/*
+你说的对，但是
+[{
+	"resource": "/d:/cppjr/MinGW-7.3.0-Windows/mingw64/x86_64-w64-mingw32/exec/.minecraft/versions/Lustful Sky/kubejs/client_scripts/jsconfig.json",
+	"owner": "typescript",
+	"severity": 8,
+	"message": "在配置文件“d:/cppjr/MinGW-7.3.0-Windows/mingw64/x86_64-w64-mingw32/exec/.minecraft/versions/Lustful Sky/kubejs/client_scripts/jsconfig.json”中找不到任何输入。指定的 \"include\" 路径为“[\"./**.js\",\"./**.ts\"]”，\"exclude\" 路径为“[]”。",
+}]
+*/
--- kubejs/server_scripts/t1/recipe_rg.js
+++ kubejs/server_scripts/t1/recipe_rg.js
@@ -20,18 +20,18 @@ CustomMachineryEvents.recipeFunction("t1.rg.generate", e => {
     const targetPos = basePos.below();
     const riftBlock = Block.getBlock("kubejs:dimrift").defaultBlockState();
     
-    //console.log(`[RG] 开始在位置 ${getXYZ(targetPos)} 放置方块`);
+    //console.log(`[RG/BlockPlace] 开始在位置 ${getXYZ(targetPos)} 放置方块`);
     for (let xOffset = -1; xOffset <= 1; xOffset++) {
         for (let zOffset = -1; zOffset <= 1; zOffset++) {
             var placePos = targetPos.offset(xOffset, 0, zOffset);
-            //console.log(`[RG] 尝试放置方块！ at ${getXYZ(placePos)}`);
+            //console.log(`[RG/BlockPlace] 尝试放置方块！ at ${getXYZ(placePos)}`);
             try {
                 world.setBlock(placePos, riftBlock, 3);
             } catch (error) {
-                //console.error(`[RG]放置方块失败！ at ${getXYZ(placePos)}: ${error}`);
+                //console.error(`[RG/BlockPlace]放置方块失败！ at ${getXYZ(placePos)}: ${error}`);
             }
         }
     }
-    //console.log(`[RG] 成功放置了 ${count} 个方块`);
+    //console.log(`[RG/BlockPlace] 成功放置了 ${count} 个方块`);
     e.success();
 })
